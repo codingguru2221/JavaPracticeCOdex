9cb1e4f0d9f7c5bc7c850676cdc6dcbbfb277658
the_codex_ <138112742+codingguru2221@users.noreply.github.com>
2025-08-19 20:04:43 +0530

    Problem 1 added

--- Code/JavaPatternProblem/src/com/pattern/Pattern_01.java
+++ Code/JavaPatternProblem/src/com/pattern/Pattern_01.java
@@ -1,4 +1,37 @@
 package com.pattern;
 
+class Day_01{
+    void Problem_01(){
+        for (int i=0; i<=5; i++){
+            for(int j=5; j>=i; --j){
+                System.out.print("  ");
+            }
+            for(int k=1; k<=i; ++k){
+                System.out.print("* ");
+            }
+            for(int k1=1; k1<=i+1; ++k1){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=1; i<=5; i++){
+            for(int j=0; j<=i; ++j){
+                System.out.print("  ");
+            }
+            for(int k=5; k>=i; --k){
+                System.out.print("* ");
+            }
+            for(int k1=5; k1>=i+1; --k1){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 public class Pattern_01 {
+    public static void main(String[] args) {
+        Day_01 D1 = new Day_01();
+        D1.Problem_01();
+    }
 }
